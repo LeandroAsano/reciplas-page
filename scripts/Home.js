309ee4519ce1181a769f158976cf6c5b941f823a
leandro.asano <leandroasano@gmail.com>
2020-05-03 11:22:42 -0300

    sidebar check removed - sidebar open onmousevoer

--- scripts/Home.js
+++ scripts/Home.js
@@ -1,27 +1,49 @@
+function showSideBar() {
+  var chk = document.getElementById("check");
+  var sidebarOpen = document.getElementById("sidebar-expanded");
+  var sidebarHide = document.getElementById("sidebar-hide");
+  var sidebarUserImg = document.getElementById("hideUserImg");
 
-function showSideBar(){
+  sidebarUserImg.style.transition = "all .5s";
 
-    var chk = document.getElementById("check")
-    var sidebarOpen = document.getElementById("sidebar-expanded")
-    var sidebarHide = document.getElementById("sidebar-hide")
-    var sidebarUserImg = document.getElementById("hideUserImg")
+  //expanded
+  if (chk.checked == true) {
+    sidebarOpen.style.left = "0px";
+    sidebarHide.style.transition = "all .5s ease";
+    sidebarHide.style.visibility = "hidden";
+    //sidebarUserImg.style.left = "15px";
+    //sidebarUserImg.style.visibility = "hidden";
 
-    sidebarUserImg.style.transition = "all .5s";
+    //hidden
+  } else {
+    sidebarOpen.style.left = "-250px";
+    sidebarHide.style.visibility = "visible";
+    //sidebarUserImg.style.left = "15px";
+  }
+}
 
-        //expanded
-    if (chk.checked == true) {
-        sidebarOpen.style.left = "0px";
-        sidebarHide.style.transition = "all .5s ease";
-        sidebarHide.style.visibility = "hidden";
-        //sidebarUserImg.style.left = "15px";
-        sidebarUserImg.style.visibility = "hideden";
+function showBar() {
+  var chk = document.getElementById("check");
+  var sidebarOpen = document.getElementById("sidebar-expanded");
+  var sidebarHide = document.getElementById("sidebar-hide");
+  var sidebarUserImg = document.getElementById("hideUserImg");
 
-        //hidden
-    } else {
-        sidebarOpen.style.left = "-250px";
-        sidebarHide.style.visibility = "visible";
-        //sidebarUserImg.style.left = "15px";
-        
-    }
+  sidebarUserImg.style.transition = "all .5s";
 
+  sidebarOpen.style.left = "0px";
+  sidebarHide.style.transition = "all .5s ease";
+  sidebarHide.style.visibility = "hidden";
+}
+
+function hideBar() {
+  var chk = document.getElementById("check");
+  var sidebarOpen = document.getElementById("sidebar-expanded");
+  var sidebarHide = document.getElementById("sidebar-hide");
+  var sidebarUserImg = document.getElementById("hideUserImg");
+
+  sidebarUserImg.style.transition = "all .5s";
+
+  sidebarOpen.style.left = "-250px";
+  sidebarHide.style.visibility = "visible";
+  //sidebarUserImg.style.left = "15px";
 }
